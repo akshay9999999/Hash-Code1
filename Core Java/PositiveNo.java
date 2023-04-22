9760b8e2c07f436727b16195b2e9c77439d341f6
Akshay Jagtap <130789998+akshay9999999@users.noreply.github.com>
2023-04-22 16:12:32 +0530

    Add files via upload

---
+++ Core Java/PositiveNo.java
@@ -0,0 +1,19 @@
+class Demo221
+{
+	public static void main(String[]args)
+	{
+		int a = 10;
+		if (a>0)
+		{
+			System.out.println("Positive no");
+		}
+		else if (a<10)
+		{
+			System.out.println("Negative no");
+		}
+		else 
+		{
+			System.out.println("Zero");
+		}
+	}
+}
